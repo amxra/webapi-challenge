e5759e04ddacad327748af90c4939bd170459060
amxra <amiradediran@gmail.com>
2019-11-01 11:04:16 +0100

    Create endpoint to retrieve project from database

--- project.js
+++ project.js
@@ -2,3 +2,22 @@ const express = require('express');
 const db = require('./data/helpers/projectModel')
 
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    db.get()
+    .then(project =>{
+        if(project){
+            res.status(200).json(project)
+        }
+        else{
+            res.status(404).json({
+                message: "The project does not exist"
+            })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({
+            error: "The project information could not be retrieved"
+        })
+    })
+})
