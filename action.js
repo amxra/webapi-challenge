462f9e5080c8e023e870456dc153e69d3264f6c2
amxra <amiradediran@gmail.com>
2019-11-01 11:37:34 +0100

    Create endpoint to get action

--- action.js
+++ action.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const db = require('./data/helpers/actionModel')
 
+const router = express.Router()
+
+router.get('/', (req,res) => {
+    db.get()
+    .then(action => {
+        if(action){
+            res.status(200).json(action)
+        }
+        else{
+            res.status(404).json({
+                message: 'Action cannot be executed'
+            })
+        }
+    })
+    .catch(() => {
+        res.status(500).json({
+            error: 'The action could not be found'
+        })
+    })
+})
