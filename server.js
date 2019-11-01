6505ea422bbf0d269d5f6a9b6d391c09661c505f
amxra <amiradediran@gmail.com>
2019-11-01 10:42:57 +0100

    Plug in cors, express, project and action in server.js

--- server.js
+++ server.js
@@ -5,3 +5,13 @@ const action = require('./action');
 const project = require('./project');
 
 const server = express()
+
+server.use(cors());
+server.use(express.json())
+server.use('/api/project', project)
+server.use('/api/action', action)
+
+server.get('/', (req,res) => {
+    res.json('Hello World')
+})
+
